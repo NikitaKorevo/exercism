28b31362940b56b599f29cf5f438c48fbec61c32
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2025-12-28 16:49:21 +0300

    refactor: use windows and any methods

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -11,22 +11,19 @@ pub fn sublist(first_list: &[i32], second_list: &[i32]) -> Comparison {
         return Comparison::Equal;
     }
 
-    let first_list: String = first_list
-        .iter()
-        .map(|number: &i32| String::from("_") + &i32::to_string(number) + "_")
-        .collect();
-    let second_list: String = second_list
-        .iter()
-        .map(|number: &i32| String::from("_") + &i32::to_string(number) + "_")
-        .collect();
-
-    println!("{}", first_list);
-
-    if first_list.contains(&second_list) {
+    if second_list.is_empty()
+        || first_list
+            .windows(second_list.len())
+            .any(|elements| elements == second_list)
+    {
         return Comparison::Superlist;
     }
 
-    if second_list.contains(&first_list) {
+    if first_list.is_empty()
+        || second_list
+            .windows(first_list.len())
+            .any(|elements| elements == first_list)
+    {
         return Comparison::Sublist;
     }
 
