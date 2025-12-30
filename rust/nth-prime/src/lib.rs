9cc60ddeb74501cf774285bb5c076ed892a8a521
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2025-12-30 13:20:24 +0300

    refactor: rename variables

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,24 +1,24 @@
 pub fn nth(n: u32) -> u32 {
-    let mut simple_numbers: Vec<u32> = vec![];
+    let mut prime_numbers: Vec<u32> = vec![];
 
     for number in 2..u32::MAX {
-        let mut is_simple_numbers = true;
+        let mut is_prime_number = true;
 
         for index in 2..=number / 2 {
             if number % index == 0 {
-                is_simple_numbers = false;
+                is_prime_number = false;
                 break;
             }
         }
 
-        if is_simple_numbers {
-            simple_numbers.push(number);
+        if is_prime_number {
+            prime_numbers.push(number);
 
-            if simple_numbers.len() > n as usize {
+            if prime_numbers.len() > n as usize {
                 break;
             }
         }
     }
 
-    simple_numbers[n as usize]
+    prime_numbers[n as usize]
 }
