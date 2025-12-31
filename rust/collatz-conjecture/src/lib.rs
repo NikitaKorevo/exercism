178d210af1753e62219429fc749ce0e030aac6c5
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2025-12-31 17:42:58 +0300

    feat: solve exercise "Collatz Conjecture"

---
+++ rust/collatz-conjecture/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+
+    let mut counter: u64 = 0;
+
+    loop {
+        if n <= 1 {
+            break;
+        }
+
+        if n.is_multiple_of(2) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1
+        }
+
+        counter += 1;
+    }
+
+    Some(counter)
+}
