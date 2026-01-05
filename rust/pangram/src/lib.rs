74f6aaec21b198719041fc112b39dfdbcf72f193
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-01-05 23:17:39 +0300

    refactor: remove variable declaration

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -2,11 +2,11 @@ use std::collections::HashSet;
 
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let hash_set: HashSet<char> = HashSet::from_iter(
+    26 == HashSet::<char>::from_iter(
         sentence
             .to_lowercase()
             .chars()
             .filter(|char| char.is_ascii_alphabetic()),
-    );
-    hash_set.len() == 26
+    )
+    .len()
 }
