205ace764838f72545515d9e0cd6fec5f2d8eeb4
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-01-11 22:07:29 +0300

    feat: use entry and filter methods instead of insert and fold

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -9,19 +9,11 @@ pub fn word_count(words: &str) -> HashMap<String, u32> {
         .map(|word| {
             word.trim_matches('\'')
                 .chars()
-                .fold(String::new(), |acc, char| match char {
-                    '\'' => acc + &char.to_string(),
-                    _ if char.is_ascii_digit() => acc + &char.to_string(),
-                    _ if char.is_ascii_alphabetic() => acc + &char.to_string(),
-                    _ => acc,
-                })
+                .filter(|char| *char == '\'' || char.is_ascii_digit() || char.is_ascii_alphabetic())
+                .collect()
         })
         .fold(HashMap::new(), |mut result, word| {
-            result.insert(
-                word.to_string(),
-                result.get(&word).cloned().unwrap_or(0) + 1,
-            );
-
+            *result.entry(word).or_insert(0) += 1;
             result
         })
 }
