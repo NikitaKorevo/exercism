6baa6e358b2a1cf18c3bf6427ba8c338fb3b50d5
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-02-21 16:29:17 +0300

    feat: solve exercise "All Your Base"

---
+++ rust/all-your-base/src/lib.rs
@@ -0,0 +1,71 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    InvalidInputBase,
+    InvalidOutputBase,
+    InvalidDigit(u32),
+}
+
+///
+/// Convert a number between two bases.
+///
+/// A number is any slice of digits.
+/// A digit is any unsigned integer (e.g. u8, u16, u32, u64, or usize).
+/// Bases are specified as unsigned integers.
+///
+/// Return the corresponding Error enum if the conversion is impossible.
+///
+///
+/// You are allowed to change the function signature as long as all test still pass.
+///
+///
+/// Example:
+/// Input
+///   number: &[4, 2]
+///   from_base: 10
+///   to_base: 2
+/// Result
+///   Ok(vec![1, 0, 1, 0, 1, 0])
+///
+/// The example corresponds to converting the number 42 from decimal
+/// which is equivalent to 101010 in binary.
+///
+///
+/// Notes:
+///  * The empty slice ( "[]" ) is equal to the number 0.
+///  * Never output leading 0 digits, unless the input number is 0, in which the output must be `[0]`.
+///    However, your function must be able to process input with leading 0 digits.
+///
+pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
+    if from_base <= 1 {
+        return Err(Error::InvalidInputBase);
+    }
+
+    if to_base <= 1 {
+        return Err(Error::InvalidOutputBase);
+    }
+
+    let mut result: Vec<u32> = vec![];
+    let mut number_10: Vec<u32> = vec![];
+
+    for (index, number) in number.iter().rev().enumerate() {
+        if *number >= from_base {
+            return Err(Error::InvalidDigit(*number));
+        }
+
+        number_10.push(number * from_base.pow(index as u32));
+    }
+
+    let mut number: u32 = number_10.iter().sum();
+
+    loop {
+        if number >= to_base {
+            result.insert(0, number % to_base);
+            number /= to_base;
+        } else {
+            result.insert(0, number);
+            break;
+        }
+    }
+
+    Ok(result)
+}
